7fbbd324d35ded981e2146b2f778a0da8ac8ae12
Nathan <nathan.p.jawahar@gmail.com>
2025-09-28 00:07:39 -0500

    update 0.6

---
+++ hidden_coin.gd
@@ -0,0 +1,12 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+var textures = {
+	1: preload("res://blue_coin_better.png"),
+	2: preload("res://green_coin_better.png"),
+	3: preload("res://red_coin_better.png"),
+	4: preload("res://yellow_coin_better.png"),
+}
